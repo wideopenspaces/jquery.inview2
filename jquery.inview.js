1e32ed7d0497ee22be367899a28a7fc681e40e6a
Christopher Blum <christopher.blum@xing.com>
2010-11-22 20:13:25 +0100

    Unify syntax, check whether element's in viewport via interval

--- jquery.inview.js
+++ jquery.inview.js
@@ -10,8 +10,8 @@
         if (height) { return height; }
         var mode = document.compatMode;
 
-        if ( (mode || !$.support.boxModel) ) { // IE, Gecko
-            height = (mode == 'CSS1Compat') ?
+        if (mode || !$.support.boxModel) { // IE, Gecko
+            height = mode == 'CSS1Compat' ?
             document.documentElement.clientHeight : // Standards
             document.body.clientHeight; // Quirks
         }
@@ -19,23 +19,20 @@
         return height;
     }
     
-    function offsetTop(debug)
-    {
+    function offsetTop(debug) {
         // Manually calculate offset rather than using jQuery's offset
         // This works-around iOS < 4 on iPad giving incorrect value
         // cf http://bugs.jquery.com/ticket/6446#comment:9
-        var curtop = 0;
-        for (var obj = debug; obj !== null; obj = obj.offsetParent)
-        {
-            curtop += obj.offsetTop;
+        var curTop = 0;
+        for (var obj = debug; obj !== null; obj = obj.offsetParent) {
+            curTop += obj.offsetTop;
         }
-        return curtop;
+        return curTop;
     }
 
-    function check_inview()
-    {
+    function checkInView() {
         var vpH = getViewportHeight(),
-            scrolltop = (window.pageYOffset ?
+            scrollTop = (window.pageYOffset ?
                 window.pageYOffset : 
                 document.documentElement.scrollTop ?
                 document.documentElement.scrollTop :
@@ -48,7 +45,7 @@
                 elems.push(this.handle.elem);
             }
         });
-
+        
         if (elems.length) {
             $(elems).each(function () {
                 var $el = $(this),
@@ -56,29 +53,25 @@
                     height = $el.height(),
                     inview = $el.data('inview') || false;
 
-                if (scrolltop > (top + height) || scrolltop + vpH < top) {
+                if (scrollTop > (top + height) || scrollTop + vpH < top) {
                     if (inview) {
                         $el.data('inview', false);
-                        $el.trigger('inview', [ false ]);                        
+                        $el.trigger('inview', [false]);                        
                     }
-                } else if (scrolltop < (top + height)) {
-                    var visPart = ( scrolltop > top ? 'bottom' : (scrolltop + vpH) < (top + height) ? 'top' : 'both' );
+                } else if (scrollTop < (top + height)) {
+                    var visPart = (scrollTop > top ? 'bottom' : (scrollTop + vpH) < (top + height) ? 'top' : 'both');
                     if (!inview || inview !== visPart) {
-                      $el.data('inview', visPart);
-                      $el.trigger('inview', [ true, visPart]);
+                        $el.data('inview', visPart);
+                        $el.trigger('inview', [true, visPart]);
                     }
                 }
             });
         }
     }
-
-    $(window).scroll(check_inview);
-    $(window).resize(check_inview);
-    $(window).click(check_inview);
     
-    // kick the event to pick up any elements already in view.
-    // note however, this only works if the plugin is included after the elements are bound to 'inview'
-    $(function () {
-        check_inview();
-    });
+    // Use setInterval in order to also make sure this captures elements within
+    // "overflow:scroll" elements
+    // old:
+    // $(window).scroll(checkInView);
+    setInterval(checkInView, 250);
 })(jQuery);
