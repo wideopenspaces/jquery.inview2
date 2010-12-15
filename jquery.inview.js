66ecd439ffce707e2ff237f2292f8ec4a0e7b724
Christopher Blum <christopher.blum@xing.com>
2010-12-15 22:54:33 +0100

    More tests (for covering visiblePart parameters), more examples, better README, load bleeding edge QUnnit version directly from github

--- jquery.inview.js
+++ jquery.inview.js
@@ -6,7 +6,7 @@
 (function ($) {
     function getViewportSize() {
         var mode, domObject, size = { height: window.innerHeight, width: window.innerWidth };
-        
+
         // if this is correct then return it. iPad has compat Mode, so will
         // go into check clientHeight/clientWidth (which has the wrong value).
         if (!size.height) {
@@ -24,46 +24,31 @@
 
         return size;
     }
-    
+
     function getViewportOffset() {
         return {
             top:  window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop,
             left: window.pageXOffset || document.documentElement.scrollLeft || document.body.scrollLeft
         };
     }
-    
+
     function getElementSize($elem) {
         return {
             height: $elem.height(),
             width:  $elem.width()
         };
     }
-    
-    // This method might fix problems on the iPad but therefore fails with elements within
-    // overflow: scroll; elements
-    //
-    // function getElementOffset(debug) {
-    //     // Manually calculate offset rather than using jQuery's offset
-    //     // This works-around iOS < 4 on iPad giving incorrect value
-    //     // cf http://bugs.jquery.com/ticket/6446#comment:9
-    //     var obj, offset = { top: 0, left: 0 };
-    //     for (obj = debug; obj !== null; obj = obj.offsetParent) {
-    //         offset.top  += obj.offsetTop;
-    //         offset.left += obj.offsetLeft;
-    //     }
-    //     return offset;
-    // }
 
     function checkInView() {
         var viewport, scrollTop, scrollLeft, elems = [];
-        
+
         // naughty, but this is how it knows which elements to check for
         $.each($.cache, function() {
             if (this.events && this.events.inview) {
                 elems.push(this.handle.elem);
             }
         });
-        
+
         if (elems.length) {
             viewportSize   = getViewportSize();
             viewportOffset = getViewportOffset();
@@ -73,36 +58,36 @@
                 if (!$.contains(document.documentElement, this)) {
                   return;
                 }
-                
+
                 var $el           = $(this),
                     elementSize   = { height: $el.height(), width: $el.width() },
                     elementOffset = $el.offset(),
                     inView        = $el.data('inview'),
-                    visiblePartY,
                     visiblePartX,
+                    visiblePartY,
                     visiblePartsMerged;
-                
+
                 if (elementOffset.top + elementSize.height > elementOffset.top &&
                     elementOffset.top < viewportOffset.top + viewportSize.height &&
                     elementOffset.left + elementSize.width > viewportOffset.left &&
                     elementOffset.left < viewportOffset.left + viewportSize.width) {
-                    visiblePartY = (viewportOffset.top > elementOffset.top ?
-                        'bottom' : (viewportOffset.top + viewportSize.height) < (elementOffset.top + elementSize.height) ?
-                        'top' : 'both');
                     visiblePartX = (viewportOffset.left > elementOffset.left ?
                         'right' : (viewportOffset.left + viewportSize.width) < (elementOffset.left + elementSize.width) ?
                         'left' : 'both');
+                    visiblePartY = (viewportOffset.top > elementOffset.top ?
+                        'bottom' : (viewportOffset.top + viewportSize.height) < (elementOffset.top + elementSize.height) ?
+                        'top' : 'both');
                     visiblePartsMerged = visiblePartX + "-" + visiblePartY;
                     if (!inView || inView !== visiblePartsMerged) {
                         $el.data('inview', visiblePartsMerged).trigger('inview', [true, visiblePartX, visiblePartY]);
                     }
                 } else if (inView) {
-                  $el.data('inview', false).trigger('inview', [false]);                        
+                  $el.data('inview', false).trigger('inview', [false]);
                 }
             });
         }
     }
-    
+
     // Use setInterval in order to also make sure this captures elements within
     // "overflow:scroll" elements or elements that appeared in the dom tree due to
     // dom manipulation and reflow
--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 module('jquery.inview', {
   setup: function() {
     $(window).scrollTop(0).scrollLeft(0);
-    
+
     this.size    = 20000;
     this.container = $('<div>');
     this.element = $('<div>', {
@@ -13,10 +13,10 @@ module('jquery.inview', {
       position: 'absolute'
     });
   },
-  
+
   teardown: function() {
     $(window).scrollTop(0).scrollLeft(0);
-    
+
     this.container.remove();
     this.element.remove();
   }
@@ -26,31 +26,31 @@ module('jquery.inview', {
 test('Check vertical scrolling', function() {
   expect(5);
   stop(10000);
-  
+
   var element = this.element,
       firstCall,
       secondCall,
       thirdCall,
       inView;
-  
+
   element.css({ left: 0, top: this.size - 50 + 'px' });
   element.appendTo('body');
   element.bind('inview.firstCall', function() { firstCall = true; });
-  
+
   setTimeout(function() {
     $(window).scrollTop(0).scrollLeft(0);
-    
+
     ok(!firstCall, 'inview shouldn\'t be triggered initially when the element isn\'t in the viewport');
     element.unbind('inview.firstCall');
     element.bind('inview.secondCall', function(event, inViewParam) {
       secondCall = true;
       inView = inViewParam;
     });
-    
+
     $(window).scrollTop(9999999);
-    
+
     setTimeout(function() {
-      
+
       ok(secondCall, 'Triggered handler after element appeared in viewport');
       ok(inView, 'Parameter, indicating whether the element is in the viewport, is set to "true"');
       element.unbind('inview.secondCall');
@@ -58,17 +58,17 @@ test('Check vertical scrolling', function() {
         thirdCall = true;
         inView = inViewParam;
       });
-      
+
       $(window).scrollTop(0).scrollLeft(0);
-      
+
       setTimeout(function() {
         ok(thirdCall, 'Triggered handler after element disappeared in viewport');
         strictEqual(inView, false, 'Parameter, indicating whether the element is in the viewport, is set to "false"');
         start();
       }, 1000);
-      
+
     }, 1000);
-    
+
   }, 1000);
 });
 
@@ -76,31 +76,31 @@ test('Check vertical scrolling', function() {
 test('Check horizontal scrolling', function() {
   expect(5);
   stop(10000);
-  
+
   var element = this.element,
       firstCall,
       secondCall,
       thirdCall,
       inView;
-  
+
   element.css({ top: 0, left: this.size - 50 + 'px' });
   element.appendTo('body');
   element.bind('inview.firstCall', function() { firstCall = true; });
-  
+
   setTimeout(function() {
     $(window).scrollTop(0).scrollLeft(0);
-    
+
     ok(!firstCall, 'inview shouldn\'t be triggered initially when the element isn\'t in the viewport');
     element.unbind('inview.firstCall');
     element.bind('inview.secondCall', function(event, inViewParam) {
       secondCall = true;
       inView = inViewParam;
     });
-    
+
     $(window).scrollLeft(9999999);
-    
+
     setTimeout(function() {
-      
+
       ok(secondCall, 'Triggered handler after element appeared in viewport');
       ok(inView, 'Parameter, indicating whether the element is in the viewport, is set to "true"');
       element.unbind('inview.secondCall');
@@ -108,17 +108,17 @@ test('Check horizontal scrolling', function() {
         thirdCall = true;
         inView = inViewParam;
       });
-      
+
       $(window).scrollTop(0).scrollLeft(0);
-      
+
       setTimeout(function() {
         ok(thirdCall, 'Triggered handler after element disappeared in viewport');
         strictEqual(inView, false, 'Parameter, indicating whether the element is in the viewport, is set to "false"');
         start();
       }, 1000);
-      
+
     }, 1000);
-    
+
   }, 1000);
 });
 
@@ -126,44 +126,96 @@ test('Check horizontal scrolling', function() {
 test('Move element into viewport without scrolling', function() {
   expect(3);
   stop(10000);
-  
+
   var element = this.element, calls = 0;
-  
+
   element
     .css({ left: '-500px', top: 0 })
     .appendTo('body')
     .bind('inview', function(event) { calls++; });
-  
+
   setTimeout(function() {
-    
+
     equals(calls, 0, 'Callback hasn\'t been fired since the element isn\'t in the viewport');
     element.css({ left: 0 });
-    
+
     setTimeout(function() {
-      
+
       equals(calls, 1, 'Callback has been fired after the element appeared in the viewport');
       element.css({ left: '10000px' });
-      
+
       setTimeout(function() {
-        
+
         equals(calls, 2, 'Callback has been fired after the element disappeared from viewport');
         start();
-        
+
       }, 1000);
-      
+
     }, 1000);
-    
+
   }, 1000);
 });
 
 
 test('Check whether element which isn\'t in the dom tree triggers the callback', function() {
   expect(0);
-  
+  stop(2000);
+
   this.element.bind('inview', function(event, isInView) {
     ok(false, 'Callback shouldn\'t be fired since the element isn\'t even in the dom tree');
     start();
   });
-  
+
   setTimeout(function() { start(); }, 1000);
+});
+
+
+test('Check visiblePartX & visiblePartY parameters #1', function() {
+  expect(2);
+  stop(2000);
+
+  this.element.css({
+    right: '-25px',
+    bottom: '-25px'
+  }).appendTo('body');
+
+  this.element.bind('inview', function(event, isInView, visiblePartX, visiblePartY) {
+    equals(visiblePartX, 'left', 'visiblePartX has correct value');
+    equals(visiblePartY, 'top', 'visiblePartX has correct value');
+    start();
+  });
+});
+
+
+test('Check visiblePartX & visiblePartY parameters #2', function() {
+  expect(2);
+  stop(2000);
+
+  this.element.css({
+    right: '0',
+    bottom: '-25px'
+  }).appendTo('body');
+
+  this.element.bind('inview', function(event, isInView, visiblePartX, visiblePartY) {
+    equals(visiblePartX, 'both', 'visiblePartX has correct value');
+    equals(visiblePartY, 'top', 'visiblePartX has correct value');
+    start();
+  });
+});
+
+
+test('Check visiblePartX & visiblePartY parameters #3', function() {
+  expect(2);
+  stop(2000);
+
+  this.element.css({
+    right: '0',
+    bottom: '0'
+  }).appendTo('body');
+
+  this.element.bind('inview', function(event, isInView, visiblePartX, visiblePartY) {
+    equals(visiblePartX, 'both', 'visiblePartX has correct value');
+    equals(visiblePartY, 'both', 'visiblePartX has correct value');
+    start();
+  });
 });
