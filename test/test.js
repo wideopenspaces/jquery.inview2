e6bd9029c92fddaa7e94ff6ac225cefe566a0d05
tiff <christopher.blum@spellboy.com>
2012-09-03 11:53:36 +0200

    Add jquery 1.8 to unit tests

--- test/test.js
+++ test/test.js
@@ -1,6 +1,6 @@
 QUnit.config.reorder = false;
 
-window['jQuery 1.6'].each(['jQuery 1.4', 'jQuery 1.5', 'jQuery 1.6', 'jQuery 1.7'], function(i, version) {
+window['jQuery 1.6'].each(['jQuery 1.4', 'jQuery 1.5', 'jQuery 1.6', 'jQuery 1.7', 'jQuery 1.8'], function(i, version) {
   var jQuery  = window[version],
       $       = jQuery;
 
